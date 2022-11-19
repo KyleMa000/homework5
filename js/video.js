02b1bc580c3cdad91a658fca35e6d113d935ad58
KyleMa000 <hm1920@nyu.edu>
2022-11-18 23:05:39 -0500

    modify1

--- js/video.js
+++ js/video.js
@@ -61,19 +61,19 @@ document.getElementById("skip").addEventListener("click", skipVid);
 
 //Mute
 function muteVid(){
-    var current = document.getElementById("mute").textContent;
+    var current = document.getElementById("mute").innerHTML;
     if(current == "Mute"){
         vid.muted = true;
-        document.getElementById("mute").textContent = "Unmute";
+        document.getElementById("mute").innerHTML = "Unmute";
         //set slider and text
-        document.getElementById("volume").textContent = "0%";
+        document.getElementById("volume").innerHTML = "0%";
         document.getElementById("slider").value = 0;
     }
     else{
         vid.muted = false;
-        document.getElementById("mute").textContent = "Mute";
+        document.getElementById("mute").innerHTML = "Mute";
         //set slider and text
-        document.getElementById("volume").textContent = vid.volume * 100 + "%";
+        document.getElementById("volume").innerHTML = vid.volume * 100 + "%";
         document.getElementById("slider").value = vid.volume * 100;
     }
 }
@@ -84,7 +84,7 @@ document.getElementById("mute").addEventListener("click", muteVid);
 //Volume Slider
 function newspeed(){
     vid.volume = this.value / 100;
-    document.getElementById("volume").textContent = vid.volume * 100 + "%";
+    document.getElementById("volume").innerHTML = vid.volume * 100 + "%";
 }
 
 document.getElementById("slider").addEventListener("change", newspeed);
